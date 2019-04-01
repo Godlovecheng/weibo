96d27150b3c2c774e4b599b7b4521ee5a7694e24
lihaochuan <lihaochuan@lihaochuandeiMac.local>
2019-04-01 16:57:39 +0800

    基础页面

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,17 @@
+{{--<!DOCTYPE htm>--}}
+{{--<html>--}}
+{{--<head>--}}
+    {{--<title>Weibo App</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--<h1>主页</h1>--}}
+{{--</body>--}}
+{{--</html>--}}
+
+@extends('layouts.default')
+
+@section('title', 'Weibo App')
+
+@section('content')
+    <h1>主页</h1>
+@stop
