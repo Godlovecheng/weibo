2ff40f4a654786cd7f5846411065b076652eaca1
lihaochuan <lihaochuan@lihaochuandeiMac.local>
2019-04-10 18:29:40 +0800

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-           'except' => ['show', 'create', 'store']
+           'except' => ['show', 'create', 'store', 'index'],
         ]);
 
         $this->middleware('guest', [
@@ -83,5 +83,13 @@ class UsersController extends Controller
         //分页
         $users = User::paginate(5);
         return view('users.index', compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
